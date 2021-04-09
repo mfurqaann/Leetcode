bd02bbb0c59ac9ce63de7c3ea2c1c09402f42268
mfurqaann <44994830+mfurqaann@users.noreply.github.com>
2021-04-09 13:18:45 +0700

    Create 1431_Kids_With_the_Greatest_Number_of_Candies.py

---
+++ Array/1431_Kids_With_the_Greatest_Number_of_Candies.py
@@ -0,0 +1,26 @@
+# Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+
+# For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. 
+# Notice that multiple kids can have the greatest number of candies.
+
+# Example 1:
+
+# Input: candies = [2,3,5,1,3], extraCandies = 3
+# Output: [true,true,true,false,true] 
+# Explanation: 
+# Kid 1 has 2 candies and if he or she receives all extra candies (3) will have 5 candies --- the greatest number of candies among the kids. 
+# Kid 2 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number of candies among the kids. 
+# Kid 3 has 5 candies and this is already the greatest number of candies among the kids. 
+# Kid 4 has 1 candy and even if he or she receives all extra candies will only have 4 candies. 
+# Kid 5 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number of candies among the kids. 
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        arr = []
+        can = sorted(candies)
+        for i in range (len(can)):
+            if candies[i] + extraCandies >= can[-1]:
+                arr.append(True)
+            else:
+                arr.append(False)
+        return arr
