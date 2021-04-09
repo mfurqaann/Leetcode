b0459a249ff67200ba0db63c50ec40a53fe1344e
mfurqaann <44994830+mfurqaann@users.noreply.github.com>
2021-04-09 13:33:46 +0700

    Create 509. Fibonacci Number.py

---
+++ Array/509. Fibonacci Number.py
@@ -0,0 +1,19 @@
+# The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+# F(0) = 0, F(1) = 1
+# F(n) = F(n - 1) + F(n - 2), for n > 1.
+# Given n, calculate F(n).
+
+ 
+
+# Example 1:
+
+# Input: n = 2
+# Output: 1
+# Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        return self.fib(n-1) + self.fib(n-2)
